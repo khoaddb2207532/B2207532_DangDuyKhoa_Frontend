6c8350c67b29715937953c2bfc8f7e58025d4523
khoaddb2207532 <dangduykhoa9a6@gmail.com>
2025-03-14 19:08:43 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Oops! Trang bạn tìm kiếm không tồn tại. Trở về
+            <router-link to="/">trang chủ.</router-link>
+        </p>
+    </div>
+</template>
